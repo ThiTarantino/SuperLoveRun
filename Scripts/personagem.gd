5602ef6b285fe47526c5c6ce6ed4f8341da30932
ThiTarantino <thiagoatarantino@gmail.com>
2025-12-16 03:12:48 -0300

    Adicionado a troca de mapa

--- Scripts/personagem.gd
+++ Scripts/personagem.gd
@@ -1,37 +1,125 @@
 extends CharacterBody2D
-
+enum PlayerState {
+	idle,
+	walk,
+	jump,
+	duck
+}
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 80.0
 const JUMP_VELOCITY = -300.0
 
+var status: PlayerState
+
+func _ready() -> void:
+	go_to_idle_state()
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.walk:
+			walk_state()
+		PlayerState.jump:
+			jump_state()
+		PlayerState.duck:
+			duck_state()
+			
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func go_to_idle_state():
+	status = PlayerState.idle
+	anim.play("idle")
+func go_to_walk_state():
+	status = PlayerState.walk
+	anim.play("walk")
+func go_to_jump_state():
+	status = PlayerState.jump
+	anim.play("jump")
+	velocity.y = JUMP_VELOCITY
+func go_to_duck_state():
+	status = PlayerState.duck
+	anim.play("duck")
+	
+func idle_state():
+	move()
+	if velocity.x !=0:
+		go_to_walk_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+	if Input.is_action_pressed("duck"):
+		go_to_duck_state()
+		return
+		
+func walk_state():
+	move()
+	if velocity.x ==0:
+		go_to_idle_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x ==0:
+			go_to_idle_state()
+		else:
+			go_to_walk_state()
+		return
+func duck_state():
+	move()
+	if Input.is_action_just_released("duck"):
+		go_to_idle_state()
+		return
+	
+	
+	
+func move():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if is_on_floor():
-		if direction > 0:
-			anim.flip_h = false   # olhando para direita
-			anim.play("walk")
-		elif direction < 0:
-			anim.flip_h = true    # olhando para esquerda
-			anim.play("walk")
-		else:
-			anim.play("idle")
-	else:
-		anim.play("jump")
+	if direction < 0 :
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
 
 
-	move_and_slide()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+		
+
+
+
+	
+
+
+	
