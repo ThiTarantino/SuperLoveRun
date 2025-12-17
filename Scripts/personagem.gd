fa64fe0db410ac599d55b3189b19867b73d73a80
ThiTarantino <thiagoatarantino@gmail.com>
2025-12-17 01:21:40 -0300

    Adicionado pulo duplo

--- Scripts/personagem.gd
+++ Scripts/personagem.gd
@@ -3,15 +3,26 @@ enum PlayerState {
 	idle,
 	walk,
 	jump,
-	duck
+	fall,
+	duck,
+	slide
 }
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
-const SPEED = 80.0
+@export var max_speed = 80.0
+@export var acceleration = 450
+@export var decceleration = 700
+@export var slide_decceleration = 40
 const JUMP_VELOCITY = -300.0
 
+var jump_count = 0
+@export var max_jump_count = 2
+var direction = 0
 var status: PlayerState
 
+
+		
 func _ready() -> void:
 	go_to_idle_state()
 
@@ -21,13 +32,17 @@ func _physics_process(delta: float) -> void:
 		
 	match status:
 		PlayerState.idle:
-			idle_state()
+			idle_state(delta)
 		PlayerState.walk:
-			walk_state()
+			walk_state(delta)
 		PlayerState.jump:
-			jump_state()
+			jump_state(delta)
+		PlayerState.fall:
+			fall_state(delta)
 		PlayerState.duck:
-			duck_state()
+			duck_state(delta)
+		PlayerState.slide:
+			slide_state(delta)
 			
 	move_and_slide()
 
@@ -42,12 +57,27 @@ func go_to_jump_state():
 	status = PlayerState.jump
 	anim.play("jump")
 	velocity.y = JUMP_VELOCITY
+	jump_count += 1
+func go_to_fall_state():
+	status = PlayerState.fall
+	anim.play("fall")
 func go_to_duck_state():
 	status = PlayerState.duck
 	anim.play("duck")
+	set_small_collider()	
+func go_to_slide_state():
+	status = PlayerState.slide
+	anim.play("slide")
+	set_small_collider()
 	
-func idle_state():
-	move()
+func exit_from_duck_state():
+	set_large_collider()	
+func exit_from_slide_state():
+	set_large_collider()
+
+		
+func idle_state(delta):
+	move(delta)
 	if velocity.x !=0:
 		go_to_walk_state()
 		return
@@ -57,43 +87,85 @@ func idle_state():
 	if Input.is_action_pressed("duck"):
 		go_to_duck_state()
 		return
-		
-func walk_state():
-	move()
+func walk_state(delta):
+	move(delta)
 	if velocity.x ==0:
 		go_to_idle_state()
 		return
 	if Input.is_action_just_pressed("jump"):
 		go_to_jump_state()
 		return
-func jump_state():
-	move()
+	if Input.is_action_just_pressed("duck"):
+		go_to_slide_state()
+		return
+	if !is_on_floor():
+		jump_count += 1
+		go_to_fall_state()
+		return	
+func jump_state(delta):
+	move(delta)
+	if Input.is_action_just_pressed("jump") && can_jump():
+		go_to_jump_state()
+		return
+	if velocity.y > 0:
+		go_to_fall_state()
+	return	
+func fall_state(delta):
+	move(delta)
+	if Input.is_action_just_pressed("jump") && can_jump():
+		go_to_jump_state()
+		return
 	if is_on_floor():
+		jump_count = 0
 		if velocity.x ==0:
 			go_to_idle_state()
 		else:
 			go_to_walk_state()
 		return
-func duck_state():
-	move()
+func duck_state(_delta):
+	update_direction()
 	if Input.is_action_just_released("duck"):
+		exit_from_duck_state()
 		go_to_idle_state()
 		return
+func slide_state(delta):
+	velocity.x = move_toward(velocity.x, 0, slide_decceleration * delta )
+	if Input.is_action_just_released("duck"):
+		exit_from_slide_state()
+		go_to_walk_state()
+		return
+	if velocity.x == 0:
+		exit_from_slide_state()
+		go_to_duck_state()
+		return
+		
+func move(delta):
+	update_direction()
 	
-	
-	
-func move():
-	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x,direction * max_speed, acceleration * delta)
+
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, decceleration * delta)	
+	
+	
+func update_direction():
+	direction = Input.get_axis("left", "right")
 	if direction < 0 :
 		anim.flip_h = true
 	elif direction > 0:
 		anim.flip_h = false
 
-
+func can_jump() -> bool:
+	return jump_count < max_jump_count
+func set_small_collider():
+	collision_shape.shape.radius = 5
+	collision_shape.shape.height = 8
+	collision_shape.position.y = 11
+func set_large_collider():
+	collision_shape.shape.radius = 5
+	collision_shape.shape.height = 22
+	collision_shape.position.y = 4	
 
 
 
